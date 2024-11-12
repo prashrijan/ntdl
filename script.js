269d109f2f264e99252fd028c57bb7f7d53b50f1
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-12 11:45:42 +1100

    swapping task from good to bad

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const hourField = document.querySelector("#hours");
 const addButton = document.querySelector("#button");
 const entryListTable = document.querySelector("#good-list");
 const badListTable = document.querySelector("#bad-list");
+const errorMessage = document.querySelector(".error-msg");
+
+errorMessage.classList.add("d-none");
 
 const tasksList = [];
 
@@ -10,39 +13,77 @@ addButton.addEventListener("click", () => {
   let task = taskField.value;
   let hour = hourField.value;
 
-  if (!task && !hour) return;
+  if (!task && !hour) {
+    errorMessage.classList.add("d-block");
+    errorMessage.classList.remove("d-none");
+    return;
+  }
   const taskData = {
+    id: Date.now(),
     task,
     hour,
+    isBad: false,
   };
 
   tasksList.push(taskData);
-  createTableRow(tasksList);
+  createTableRow();
 
   taskField.value = "";
   hourField.value = "";
-  console.log(tasksList);
 });
 
-const createTableRow = (tasksList) => {
+const createTableRow = () => {
   entryListTable.innerHTML = "";
+  badListTable.innerHTML = "";
 
-  tasksList.forEach(({ task, hour }, index) => {
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
+  errorMessage.classList.add("d-none");
+  errorMessage.classList.remove("d-block");
+
+  tasksList.forEach(({ id, task, hour, isBad }, index) => {
+    let goodTr = document.createElement("tr");
+    goodTr.innerHTML = `
         <th scope="row">${index + 1}</th>
         <td>${task}</td>
-        <td>${hour}</td>
+        <td>${hour} hrs</td>
         <td class="text-end">
             <button type="button" class="btn btn-danger me-1">
                 <i class="ri-delete-bin-line"></i>
             </button>
-            <button type="button" class="btn btn-success">
+            <button type="button" class="btn btn-success" onclick="toggleTaskType(${id})">
                 <i class="ri-arrow-right-line"></i>
             </button>
         </td>
       `;
 
-    entryListTable.appendChild(tr);
+    let badTr = document.createElement("tr");
+    badTr.innerHTML = `
+      <th scope="row">${index + 1}</th>
+        <td>${task}</td>
+        <td>${hour} hrs</td>
+        <td class="text-end">
+            <button type="button" class="btn btn-warning" 
+            onclick="toggleTaskType(${id})">
+            <i class="ri-arrow-left-line"></i>
+            </button>
+            <button type="button" class="btn btn-danger me-1">
+                <i class="ri-delete-bin-line"></i>
+            </button>
+        </td>
+      
+      `;
+
+    if (!isBad) {
+      entryListTable.appendChild(goodTr);
+    } else {
+      badListTable.appendChild(badTr);
+    }
   });
+};
+
+const toggleTaskType = (id) => {
+  let task = tasksList.find((task) => task.id === id);
+
+  task.isBad = task.isBad === true ? false : true;
+
+  createTableRow();
 };
