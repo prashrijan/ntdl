088618583fe2c0ac66c041046f13525dd3d0841d
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-12 14:57:57 +1100

    not to do list project v1 completed

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ const addButton = document.querySelector("#button");
 const entryListTable = document.querySelector("#good-list");
 const badListTable = document.querySelector("#bad-list");
 const errorMessage = document.querySelector(".error-msg");
+const totalHours = document.querySelector("#totalHours");
+const badHours = document.querySelector("#badHours");
+const toastLiveExample = document.getElementById("liveToast");
+const toastMessage = document.getElementById("toastMessage");
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
 
 errorMessage.classList.add("d-none");
 
+const weeklyHours = 24 * 7;
+
 let tasksList = [];
 
-addButton.addEventListener("click", () => {
+const getAndDisplayList = () => {
   let task = taskField.value;
   let hour = hourField.value;
 
@@ -25,12 +32,18 @@ addButton.addEventListener("click", () => {
     isBad: false,
   };
 
+  if (parseInt(taskData.hour) + calculateTotalHours() > weeklyHours) {
+    alert(`Weekly Hours Exceeded`);
+    return;
+  }
+
   tasksList.push(taskData);
+
   createTableRow();
 
   taskField.value = "";
   hourField.value = "";
-});
+};
 
 // rerender the table row each time
 const createTableRow = () => {
@@ -40,49 +53,54 @@ const createTableRow = () => {
   errorMessage.classList.add("d-none");
   errorMessage.classList.remove("d-block");
 
-  tasksList.forEach(({ id, task, hour, isBad }, index) => {
+  tasksList.forEach(({ id, task, hour, isBad }) => {
     let goodTr = document.createElement("tr");
+    goodTr.classList.add("tr-row");
     goodTr.innerHTML = `
-        <th scope="row">${index + 1}</th>
-        <td>${task}</td>
-        <td>${hour} hrs</td>
-        <td class="text-end">
-            <button type="button" class="btn btn-danger me-1"
-            onclick="deleteTask(${id})"
-            >
-                <i class="ri-delete-bin-line"></i>
-            </button>
-            <button type="button" class="btn btn-success" onclick="toggleTaskType(${id})">
-                <i class="ri-arrow-right-line"></i>
-            </button>
-        </td>
-      `;
+    <th scope="row"><input type="checkbox" onclick="toggleLineThrough(this)"></th>
+    <td id="task">${task}</td>
+    <td id="task">${hour} hrs</td>
+    <td class="text-end">
+    <button type="button" class="btn btn-danger me-1"
+    onclick="deleteTask(${id})"
+    >
+    <i class="ri-delete-bin-line"></i>
+    </button>
+    <button type="button" class="btn btn-success" onclick="toggleTaskType(${id})">
+    <i class="ri-arrow-right-line"></i>
+    </button>
+    </td>
+    `;
 
     let badTr = document.createElement("tr");
-    badTr.innerHTML = `
-      <th scope="row">${index + 1}</th>
-        <td>${task}</td>
-        <td>${hour} hrs</td>
-        <td class="text-end">
-            <button type="button" class="btn btn-warning" 
-            onclick="toggleTaskType(${id})">
-            <i class="ri-arrow-left-line"></i>
-            </button>
-            <button type="button" class="btn btn-danger me-1"
-            onclick="deleteTask(${id})"
-            >
-                <i class="ri-delete-bin-line"></i>
-            </button>
-        </td>
-      
-      `;
 
+    badTr.innerHTML = `
+    <th scope="row"><input type="checkbox" onclick="toggleLineThrough(this)"></th>
+    <td id="task">${task}</td>
+    <td id="task">${hour} hrs</td>
+    <td class="text-end">
+    <button type="button" class="btn btn-warning" 
+    onclick="toggleTaskType(${id})">
+    <i class="ri-arrow-left-line"></i>
+    </button>
+    <button type="button" class="btn btn-danger me-1"
+    onclick="deleteTask(${id})"
+    >
+    <i class="ri-delete-bin-line"></i>
+    </button>
+    </td>
+    
+    `;
     if (!isBad) {
       entryListTable.appendChild(goodTr);
     } else {
       badListTable.appendChild(badTr);
     }
   });
+
+  totalHours.innerText = calculateTotalHours();
+
+  badHours.innerText = calculateBadHours();
 };
 
 // convert the task type
@@ -98,3 +116,48 @@ const deleteTask = (id) => {
   tasksList = tasksList.filter((task) => task.id !== id);
   createTableRow();
 };
+
+const toggleLineThrough = (checkbox) => {
+  const row = checkbox.closest("tr");
+
+  row.querySelectorAll("#task").forEach((item) => {
+    item.style.textDecoration = checkbox.checked ? "line-through" : "none";
+  });
+};
+
+const calculateTotalHours = () => {
+  let totalHour = tasksList.reduce((acc, item) => {
+    return acc + parseInt(item.hour);
+  }, 0);
+  return totalHour;
+};
+
+const calculateBadHours = () => {
+  let badHour = tasksList.reduce((acc, item) => {
+    if (item.isBad) {
+      return acc + parseInt(item.hour);
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  return badHour;
+};
+
+addButton.addEventListener("click", getAndDisplayList);
+
+addButton.addEventListener("click", () => {
+  if (tasksList.length) {
+    toastMessage.innerText = "Task Added Successfully.";
+    toastBootstrap.show();
+  } else {
+    toastMessage.innerText = "Please Enter a task.";
+    toastBootstrap.show();
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    getAndDisplayList();
+  }
+});
